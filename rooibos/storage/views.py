677db1eb9612eac2e5bd9936f41b1c06449086f1
Peter Hanley <phanley@temple.edu>
2014-05-20 09:34:01 -0400

    more of a bandaid to storage.add_content_length

--- rooibos/storage/views.py
+++ rooibos/storage/views.py
@@ -42,10 +42,10 @@ def add_content_length(func):
             elif isinstance(response._container, file):
                 if os.path.exists(response._container.name):
                     response['Content-Length'] = os.path.getsize(response._container.name)
-            #elif response._is_string:
             else:
-                logging.debug(response)
-                logging.debug(dir(response))
+                # was elif response._is_string:
+                # TODO: this doesn't seem too dangerous, right?  need to figure out what the alternatve is
+                # response._is_string was throwing an error
                 response['Content-Length'] = len(response.content)
         return response
     return _add_header
