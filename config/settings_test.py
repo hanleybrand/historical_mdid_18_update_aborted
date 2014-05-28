fad7aaeabf9cb2a17fbff0b33e97f25bd0dddc02
Peter Hanley <phanley@temple.edu>
2014-05-28 11:31:58 -0400

    switch settings_test db back to sqllite
    working around content_types caching problem with tests and mysql

--- config/settings_test.py
+++ config/settings_test.py
@@ -4,29 +4,29 @@ LOGGING_OUTPUT_ENABLED = DEBUG
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_test_mdid',
-        'USER': 'rooibos',
-        'PASSWORD': 'rooibos',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-        'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB',
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'test_test_mdid',
+#         'USER': 'rooibos',
+#         'PASSWORD': 'rooibos',
+#         'HOST': '127.0.0.1',
+#         'PORT': '',
+#         'OPTIONS': {
+#             'init_command': 'SET storage_engine=INNODB',
+#         },
+#     }
+# }
 
 # I checked to make sure that the problems weren't with mysql vs sqlite but sqlite throws amazing errors
 # due to how it handles atomic requests and manual transaction management
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'test.sqllite',
-#         'ATOMIC_REQUESTS': True
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.sqllite',
+        'ATOMIC_REQUESTS': True
+    }
+}
 
 CACHE_BACKEND = 'dummy://'
 
