f959de1076b36e96b6bbd78502da3db1804277a3
Peter Hanley <phanley@temple.edu>
2015-03-03 09:08:50 -0500

    add basic startup logging
    looks like
    ```
    ====== MDID Startup
    ========================================================================
    ==================
    [02/Mar/2015 23:33:25] INFO [rooibos:10] Important Directories
    [02/Mar/2015 23:33:25] INFO [rooibos:11] MDID3 root:
    /var/log/Dev/hacking-mdid3
    [02/Mar/2015 23:33:25] INFO [rooibos:12] config dir:
    /var/log/Dev/hacking-mdid3/config
    [02/Mar/2015 23:33:25] INFO [rooibos:13] rooibos dir:
    /var/log/Dev/hacking-mdid3/rooibos
    [02/Mar/2015 23:33:25] INFO [rooibos:14] media root:
    /var/log/Dev/mdid-data/hacking-mdid3
    [02/Mar/2015 23:33:25] INFO [rooibos:15] scratch dir:
    /var/log/Dev/mdid-data/hacking-mdid3/mdid-scratch
    [02/Mar/2015 23:33:25] INFO [rooibos:16] auto-storage:
    /var/log/Dev/mdid-data/hacking-mdid3/collections
    [02/Mar/2015 23:33:25] INFO [rooibos:17] log file:
    /var/log/Dev/mdid-data/hacking-mdid3/logs/rooibos.log
    [02/Mar/2015 23:33:25] INFO [rooibos:18] ====end startup
    messages=============================================
    ```

--- rooibos/__init__.py
+++ rooibos/__init__.py
@@ -1,1 +1,18 @@
+import logging
+import os, sys
+from config.settings import PROJECT_ROOT, BASE_DIR, ROOIBOS_ROOT, CONTRIB_DIR, STATIC_ROOT, MEDIA_ROOT
+from config.settings_local import DEBUG, AUTO_STORAGE_DIR, SCRATCH_DIR, LOGGING
+import config.settings_local
+log = logging.getLogger('rooibos')
 
+
+log.info('\n\n\n\n====== MDID Startup ===============================================================================')
+log.info('Important Directories')
+log.info('MDID3 root:     %s', PROJECT_ROOT)
+log.info('config dir:     %s', os.path.dirname(config.settings_local.__file__))
+log.info('rooibos dir:    %s', ROOIBOS_ROOT)
+log.info('media root:     %s', MEDIA_ROOT)
+log.info('scratch dir:    %s', SCRATCH_DIR)
+log.info('auto-storage:   %s', AUTO_STORAGE_DIR)
+log.info('log file:       %s', LOGGING['handlers']['file']['filename'])
+log.info('====end startup messages======================================================\n')
