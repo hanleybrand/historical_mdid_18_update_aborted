7a330e5c3539a2bd517f64f973d81517b1b2f7f8
Peter Hanley <phanley@temple.edu>
2015-03-06 17:18:12 -0500

    fixing what was probably a future bug in startup logging
    importing con.settings instead of django.conf.settings

--- rooibos/__init__.py
+++ rooibos/__init__.py
@@ -1,19 +1,17 @@
 import logging
 import os, sys
-from config.settings import PROJECT_ROOT, ROOIBOS_ROOT,  STATIC_ROOT, MEDIA_ROOT
-from config.settings_local import DEBUG, AUTO_STORAGE_DIR, SCRATCH_DIR, LOGGING
+from django.conf import settings
 import config.settings_local
 log = logging.getLogger('rooibos')
 
-
 log.info('\n\n\n\n====== MDID Startup ===============================================================================')
 log.info('Important Directories')
-log.info('MDID3 root:     %s', PROJECT_ROOT)
+log.info('MDID3 root:     %s', settings.PROJECT_ROOT)
 log.info('config dir:     %s', os.path.dirname(config.settings_local.__file__))
-log.info('rooibos dir:    %s', ROOIBOS_ROOT)
-log.info('media root:     %s', MEDIA_ROOT)
-log.info('static files:   %s', STATIC_ROOT)
-log.info('scratch dir:    %s', SCRATCH_DIR)
-log.info('auto-storage:   %s', AUTO_STORAGE_DIR)
-log.info('log file:       %s', LOGGING['handlers']['file']['filename'])
+log.info('rooibos dir:    %s', settings.ROOIBOS_ROOT)
+log.info('media root:     %s', settings.MEDIA_ROOT)
+log.info('static files:   %s', settings.STATIC_ROOT)
+log.info('scratch dir:    %s', settings.SCRATCH_DIR)
+log.info('auto-storage:   %s', settings.AUTO_STORAGE_DIR)
+log.info('log file:       %s', settings.LOGGING['handlers']['file']['filename'])
 log.info('====end startup messages======================================================\n')
