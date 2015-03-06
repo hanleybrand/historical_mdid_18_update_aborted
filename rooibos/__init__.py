23f7263be71b496f4036407f3256b7f43977eef0
Peter Hanley <phanley@temple.edu>
2015-03-06 15:52:24 -0500

    minor change to startup info dump

--- rooibos/__init__.py
+++ rooibos/__init__.py
@@ -1,6 +1,6 @@
 import logging
 import os, sys
-from config.settings import PROJECT_ROOT, BASE_DIR, ROOIBOS_ROOT, CONTRIB_DIR, STATIC_ROOT, MEDIA_ROOT
+from config.settings import PROJECT_ROOT, ROOIBOS_ROOT,  STATIC_ROOT, MEDIA_ROOT
 from config.settings_local import DEBUG, AUTO_STORAGE_DIR, SCRATCH_DIR, LOGGING
 import config.settings_local
 log = logging.getLogger('rooibos')
@@ -12,6 +12,7 @@ log.info('MDID3 root:     %s', PROJECT_ROOT)
 log.info('config dir:     %s', os.path.dirname(config.settings_local.__file__))
 log.info('rooibos dir:    %s', ROOIBOS_ROOT)
 log.info('media root:     %s', MEDIA_ROOT)
+log.info('static files:   %s', STATIC_ROOT)
 log.info('scratch dir:    %s', SCRATCH_DIR)
 log.info('auto-storage:   %s', AUTO_STORAGE_DIR)
 log.info('log file:       %s', LOGGING['handlers']['file']['filename'])
