4b5a916b48c703b3dbb6d61d044bd7233feaa2a7
Peter Hanley <phanley@temple.edu>
2015-03-08 15:39:38 -0400

    simplejson > json fix

--- rooibos/api/views.py
+++ rooibos/api/views.py
@@ -7,7 +7,6 @@ from django.db.models import Q
 from django.http import HttpResponse, HttpRequest, HttpResponseNotAllowed, HttpResponseForbidden
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.utils import simplejson
 from django.views.decorators.cache import cache_control
 from rooibos.access import filter_by_access
 from rooibos.data.models import Collection, CollectionItem, DisplayFieldValue, Field, FieldSet, FieldSetField, FieldValue, MetadataStandard, Record
