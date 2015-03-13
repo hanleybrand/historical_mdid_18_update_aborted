4f18f91d4f443c61c00efe0a6aedee9fb53f128e
Peter Hanley <phanley@temple.edu>
2015-03-13 12:22:36 -0600

    PEP-8 formatting

--- rooibos/auth/middleware.py
+++ rooibos/auth/middleware.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse, HttpResponseForbidden
 from django.conf import settings
 from django.contrib.auth import login, authenticate
 
-def basic_challenge(realm = None):
+
+def basic_challenge(realm=None):
     if realm is None:
         realm = getattr(settings, 'WWW_AUTHENTICATION_REALM', 'Restricted Access')
     # TODO: Make a nice template for a 401 message?
-    response =  HttpResponse('Authorization Required', mimetype="text/plain")
+    response = HttpResponse('Authorization Required', mimetype="text/plain")
     response['WWW-Authenticate'] = 'Basic realm="%s"' % (realm)
     response.status_code = 401
     return response
@@ -14,16 +15,15 @@ def basic_challenge(realm = None):
 
 def basic_authenticate(authentication):
     # Taken from paste.auth
-    (authmeth, auth) = authentication.split(' ',1)
+    (authmeth, auth) = authentication.split(' ', 1)
     if 'basic' != authmeth.lower():
         return None
     auth = auth.strip().decode('base64')
-    username, password = auth.split(':',1)
+    username, password = auth.split(':', 1)
     return authenticate(username=username, password=password)
 
 
 class BasicAuthenticationMiddleware:
-
     def process_request(self, request):
         if request.META.has_key('HTTP_AUTHORIZATION') and not request.user.is_authenticated():
             user = basic_authenticate(request.META['HTTP_AUTHORIZATION'])
