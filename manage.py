a4b8599d2ca3c1fbde5392d78491b2a5f4f411df
Peter Hanley <phanley@temple.edu>
2015-03-16 11:09:16 -0400

    testing drop in support for pymysql
    I feel like this would need to be added to wsgi.py in production...

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@
 import os
 import sys
 
+# enable drop in pymysql replacement of python-mysqldb
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
