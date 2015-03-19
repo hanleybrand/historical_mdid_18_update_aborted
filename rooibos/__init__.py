1b1d06f54c441c2a89cf5dd901ac92c3d5e62fe3
Peter Hanley <phanley@temple.edu>
2015-03-19 15:20:48 -0400

    fix startup logging

--- rooibos/__init__.py
+++ rooibos/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os, sys
 from django.conf import settings
 import config.settings_local
+
 log = logging.getLogger('rooibos')
 
 log.info('\n\n\n\n====== MDID Startup ===============================================================================')
@@ -13,5 +14,5 @@ log.info('media root:     %s', settings.MEDIA_ROOT)
 log.info('static files:   %s', settings.STATIC_ROOT)
 log.info('scratch dir:    %s', settings.SCRATCH_DIR)
 log.info('auto-storage:   %s', settings.AUTO_STORAGE_DIR)
-log.info('log file:       %s', settings.LOGGING['handlers']['file']['filename'])
+log.info('log file:       %s', settings.LOGGING['handlers']['rooibos']['filename'])
 log.info('====end startup messages======================================================\n')
