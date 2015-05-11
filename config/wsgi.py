9f22b777eb083d6331a151c98f2181191d1aefb5
Peter Hanley <phanley@temple.edu>
2015-05-11 17:10:31 -0400

    config/wsgi.py fixups
    supporting pymysql,  making the path additions relative

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,10 +9,19 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 import sys
+from os.path import normpath, join, dirname, exists
+
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 # TODO: are these sys.path.appends necessary? (they don't seem to be - were in mdid_dj16/dist/linux/django.wsgi file)
+sys.path.append(normpath(join(dirname(__file__), '../')))
+sys.path.append(normpath(join(dirname(__file__), '../rooibos/contrib')))
 #sys.path.append('/var/local/mdid_dj16')
 #sys.path.append('/var/local/mdid_dj16/rooibos/contrib')
 
