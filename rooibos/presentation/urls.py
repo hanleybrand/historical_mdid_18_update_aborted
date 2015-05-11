12dbdbbe4d83c95caa6c4dec5fdd817f7da019db
Peter Hanley <phanley@temple.edu>
2015-05-11 17:28:33 -0400

    for some reason django 1.8 hated this urls.py because ?
    explicit imports saved the day though. I added the view= just to be
    stone-cold explicit (and obvious, I guess)

--- rooibos/presentation/urls.py
+++ rooibos/presentation/urls.py
@@ -1,12 +1,15 @@
+from __future__ import absolute_import
+
 from django.conf.urls import *
-from views import *
+
+from .views import manage, create, edit, duplicate, browse, password, record_usage
 
 urlpatterns = patterns('',
-    url(r'^manage/$', manage, name='presentation-manage'),
-    url(r'^create/$', create, name='presentation-create'),
-    url(r'^edit/(?P<id>\d+)/(?P<name>[-\w]+)/$', edit, name='presentation-edit'),
-    url(r'^duplicate/(?P<id>\d+)/(?P<name>[-\w]+)/$', duplicate, name='presentation-duplicate'),
-    url(r'^browse/$', browse, name='presentation-browse'),
-    url(r'^password/(?P<id>\d+)/(?P<name>[-\w]+)/$', password, name='presentation-password'),
-    url(r'^record-usage/(?P<id>\d+)/(?P<name>[-\w]+)/$', record_usage, name='presentation-record-usage'),
+    url(r'^manage/$', view=manage, name='presentation-manage'),
+    url(r'^create/$', view=create, name='presentation-create'),
+    url(r'^edit/(?P<id>\d+)/(?P<name>[-\w]+)/$', view=edit, name='presentation-edit'),
+    url(r'^duplicate/(?P<id>\d+)/(?P<name>[-\w]+)/$', view=duplicate, name='presentation-duplicate'),
+    url(r'^browse/$', view=browse, name='presentation-browse'),
+    url(r'^password/(?P<id>\d+)/(?P<name>[-\w]+)/$', view=password, name='presentation-password'),
+    url(r'^record-usage/(?P<id>\d+)/(?P<name>[-\w]+)/$', view=record_usage, name='presentation-record-usage'),
 )
