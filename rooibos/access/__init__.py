fd2f4f97d6786f8b4f636ab69339239bd91a07fa
Peter Hanley <phanley@temple.edu>
2015-08-18 16:56:01 -0400

    fix linty bellyaching
    This does nothing but tries to shut up the linter that says "import *
    is bad"

--- rooibos/access/__init__.py
+++ rooibos/access/__init__.py
@@ -1,1 +1,4 @@
-from functions import *
+from __future__ import absolute_import
+from .functions import add_restriction_precedence, check_access, filter_by_access
+from .functions import get_accesscontrols_for_object, get_effective_permissions
+from .functions import get_effective_permissions_and_restrictions
