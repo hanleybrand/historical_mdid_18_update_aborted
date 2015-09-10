65e5fee1b70ecb09add402396715b963a8a43a14
Peter Hanley <phanley@temple.edu>
2015-09-09 21:45:54 -0400

    bootstrap.sh overhaul - now with 75% more working as intended
    I wish I could remember where I got the idea that vagrant provisioners
    ran as root - this script works so much better when written from the
    point of view of the vagrant. Maybe the dialogue is more plausible to
    the colonel?  Lemmie stop.

--- .vagrant_provision/bootstrap.sh
+++ .vagrant_provision/bootstrap.sh
@@ -1,90 +1,31 @@
 #!/usr/bin/env bash
 
+# export DEBIAN_FRONTEND=noninteractive
+# set -e # Exit script immediately on first error.
+set -x # Print commands and their arguments as they are executed.
+
+echo "I am" `whoami`
+
 echo -e "################# Provisioning vagrant box via ./vagrant_provision/bootstrap.sh #################"
 
-HOME_DIR=`pwd`
-VAGRANT_DIR=/vagrant
-PROVISION_DIR=$VAGRANT_DIR/.vagrant_provision
-MDID_DIR=$HOME_DIR/mdid
-MDID_DATA_DIR=$HOME_DIR/mdid-data
-CONFIG_DIR=$MDID_DIR/config
-ROOIBOS_DIR=$MDID_DIR/rooibos
+# vagrant dirs
+export HOME_DIR=/home/vagrant
+export VAGRANT_DIR=/vagrant
+export PROVISION_DIR=$VAGRANT_DIR/.vagrant_provision
+# MDID dirs
+export MDID_DIR=$HOME_DIR/mdid
+export MDID_DATA_DIR=$HOME_DIR/mdid-data
+export CONFIG_DIR=$MDID_DIR/config
+export ROOIBOS_DIR=$MDID_DIR/rooibos
 
-SQL_PASSWORD=rooibos
-
-# create a logs dir so verbose output of the script can be saved for review if something is not correct
-sudo -u vagrant mkdir -p bootstrap_logs
-
+# SQL password for root - obviously this would be stupid in a non-local non-dev context
+# (the vagrant vm is set to be private network)
+# do we need to say DO NOT DO THIS ON AN EXTERNALLY ACCESSIBLE DEVICE?
+export SQL_PASSWORD=rooibos
 
 ##############################################################################
 # A Vagrant provisioning shell script to setup an MDID Development VM
 ##############################################################################
-
-echo -e "################# Making sure system & apt-get are up to date #################"
-
-# add ppa for ffmpeg
-add-apt-repository ppa:mc3man/trusty-media 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# Update our apt sources
-apt-get update 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# Make sure we are starting from an up-to-date system
-apt-get upgrade -y 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-apt-get dist-upgrade -y 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-echo -e "################# Installing MySQL, Solr, RabbitMQ, memcached #################"
-
-# The mysql-server package interactively prompts for the root pasword to the
-# MySQL server.  These commands will cache the password settings, and they
-# will be used by the mysql-server package on installation.  Since this is
-# pretty insecure anyway, no attempt is made at using a secure password :)
-#
-# Sets the MySQL root password to 'rooibos' to conform to existing docs, etc.
-
-echo -e "\n\n                  PSSST:  the mysql root password is: rooibos\n\n"
-
-echo mysql-server mysql-server/root_password password SQL_PASSWORD | debconf-set-selections
-echo mysql-server mysql-server/root_password_again password SQL_PASSWORD | debconf-set-selections
-
-# Now we can install the packages
-# mysql-server: The MySQL server
-# mysql-client: The MySQL client utilities
-# libmysqlclient-dev: MySQL developmental libraries, needed to build the python
-#   MySQL module
-apt-get install -y mysql-server mysql-client libmysqlclient-dev 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-##############################################################################
-# Install other dependencies
-##############################################################################
-
-# Java is needed to run Solr
-apt-get install -y openjdk-7-jre-headless 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# RabbitMQ is needed to manage the worker jobs
-apt-get install -y rabbitmq-server 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# Memcached is used for django's memory object cache
-apt-get install -y memcached 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-##############################################################################
-# Python build dependencies
-##############################################################################
-
-echo -e "################# Installing python things that require apt-get #################"
-
-apt-get install -y python-dev 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# PyODBC needs the unixodbc libs
-apt-get install -y unixodbc unixodbc-dev 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# python-ldap needs ldap and sasl libraries
-apt-get install -y libldap2-dev libsasl2-dev 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# Pillow needs image libraries
-apt-get install -y libtiff5-dev libjpeg8-dev zlib1g-dev 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-
-# ffmpeg for video funage
-apt-get install -y ffmpeg 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
 
 
 ##############################################################################
@@ -93,24 +34,103 @@ apt-get install -y ffmpeg 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/inst
 
 echo -e "################# Setting up files & directories as vagrant user #################"
 
-# create a symlink from /vagrant to our home dir
-sudo -u vagrant ln -s $VAGRANT_DIR $MDID_DIR
 
-# create directories for our mdid data
-sudo -u vagrant mkdir -p $MDID_DATA_DIR
+# create a logs dir so verbose output of the script can be saved for review if something is not correct
+mkdir -p $HOME_DIR/bootstrap_logs
+mkdir -p $MDID_DATA_DIR
+mkdir -p $MDID_DATA_DIR/logs
+
+# create a ln from /vagrant to our $MDID_DIR
+ln -s $VAGRANT_DIR $MDID_DIR
+
+
 # explicitly create the log directory, because when the workers service fires
 # up later in the provisioning, if they're not there then they will get
 # created by root and permissions will be all jacked up
-sudo -u vagrant mkdir -p $MDID_DATA_DIR/scratch/logs
-
-# link in a little helper script for running the django dev server
-sudo -u vagrant ln -s $PROVISION_DIR/runserver $HOME_DIR/
-
-# touch the
+mkdir -p $MDID_DATA_DIR/scratch/logs
 
 
-sudo -u vagrant mkdir ~/mdid-data/logs/
-sudo -u vagrant touch ~/mdid-data/logs/request.log
+# touch the log files so they work later
+
+touch $MDID_DATA_DIR/logs/rooibos.log
+touch $MDID_DATA_DIR/logs/request.log
+touch $MDID_DATA_DIR/logs/sql_etc.log
+
+echo -e "################# Making sure system & apt-get are up to date #################"
+
+# Update our apt sources
+sudo apt-get update 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+# Make sure we are starting from an up-to-date system
+sudo apt-get upgrade -y 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+sudo apt-get dist-upgrade -y 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+echo -e "################# Installing MySQL, Solr, RabbitMQ, memcached #################"
+
+# The mysql-server package interactively prompts for the root pasword to the
+# MySQL server.  These commands will cache the password settings, and they
+# will be used by the mysql-server package on installation.  Since this is
+# pretty insecure anyway, no attempt is made at using a secure password :)
+#
+# Sets the MySQL root password to '$SQL_PASSWORD' to conform to existing docs, etc.
+
+echo -e "\n\n                  PSSST:  the mysql root password is: $SQL_PASSWORD\n\n"
+
+sudo echo mysql-server mysql-server/root_password password $SQL_PASSWORD | debconf-set-selections
+sudo echo mysql-server mysql-server/root_password_again password $SQL_PASSWORD | debconf-set-selections
+
+
+# Now we can install the packages
+# mysql-server: The MySQL server
+# mysql-client: The MySQL client utilities
+# libmysqlclient-dev: MySQL developmental libraries, needed to build the python
+#   MySQL module
+sudo apt-get install -y mysql-server mysql-client libmysqlclient-dev 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+
+##############################################################################
+# Install other dependencies
+##############################################################################
+
+# Java is needed to run Solr
+apt-get install -y openjdk-7-jre-headless 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+sudo mkdir -p /usr/java
+sudo ln -s /usr/lib/jvm/java-7-openjdk-amd64 /usr/java/default
+
+# Solr should probably installed, while we're at it...
+sudo apt-get install -y solr-jetty 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+# RabbitMQ is needed to manage the worker jobs
+sudo apt-get install -y rabbitmq-server 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+# Memcached is used for django's memory object cache
+sudo apt-get install -y memcached 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+# ffmpeg for video funage
+# add repo for ffmpeg and install it
+sudo add-apt-repository ppa:mc3man/trusty-media 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+sudo apt-get install -y ffmpeg 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+
+##############################################################################
+# Python build dependencies
+##############################################################################
+
+echo -e "################# Installing python things that require apt-get #################"
+
+sudo apt-get install -y python-dev 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
+
+# PyODBC needs the unixodbc libs
+sudo apt-get install -y unixodbc unixodbc-dev 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
+
+# python-ldap needs ldap and sasl libraries
+sudo apt-get install -y libldap2-dev libsasl2-dev 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
+
+# Pillow needs image libraries
+sudo apt-get install -y libtiff5-dev libjpeg8-dev zlib1g-dev 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
+
+
+
 
 ##############################################################################
 # Configure Python and setup a Virtual Environment
@@ -119,8 +139,10 @@ sudo -u vagrant touch ~/mdid-data/logs/request.log
 
 echo -e "################# Configuring Python and setting up Virtual Environment #################"
 
+sudo apt-get install -y python-pip 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
 
-apt-get install -y python-pip 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+# in case the ubuntu repo lags behind pip, upgrade it!
+sudo pip install -U pip 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
 
 echo -e "################# Installing virtualenv software #################"
 
@@ -128,58 +150,54 @@ echo -e "################# Installing virtualenv software #################"
 # as per docs, virtualenv & vewrapper should only be installed in a system install of python
 # http://virtualenvwrapper.readthedocs.org/en/latest/install.html#basic-installation
 
-WORKON_HOME=$HOME/.virtualenvs
 
-pip install --upgrade virtualenv 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
-pip install --upgrade virtualenvwrapper 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+mkdir -p $HOME_DIR/.virtualenvs
 
-sudo -u vagrant mkvirtualenv -a $MDID_DIR -r $MDID_DIR/requirements.txt mdid_env
+export WORKON_HOME=$HOME_DIR/.virtualenvs
+
+sudo pip install --upgrade virtualenv 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
+sudo pip install --upgrade virtualenvwrapper 1>> bootstrap_logs/python_install.txt 2>> bootstrap_logs/python_install_errors.txt
 
 ## add git autocomplete, git prompt and a few other niceties to the cmd line
 ## and stick in a .bash-profile to autosource them
 
-cp $PROVISION_DIR/.bash-completion $HOME_DIR
+sudo apt-get install git bash-completion 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
+# cp $PROVISION_DIR/.bash-completion $HOME_DIR
 cp $PROVISION_DIR/.git-prompt $HOME_DIR
 cp $PROVISION_DIR/.mdid-funcs $HOME_DIR
-cp $PROVISION_DIR/.bash_profile $HOME_DIR
+cp $PROVISION_DIR/.bash_aliases $HOME_DIR/
+cp $PROVISION_DIR/.my.cnf $HOME_DIR
 
+# copy a little helper script for running the django dev server
+cp $PROVISION_DIR/runserver $HOME_DIR/
 
-#source ~/.profile
-#
-#if [ -z "$VAR" ] || [ "$VAR" != "value" ]; then
-#  echo "export VAR=value" >> ~/.profile
-#fi
-#
-#if [ $(pwd) != "/vagrant" ]; then
-#  echo "cd /vagrant" >> ~/.profile
-#fi
+source /usr/local/bin/virtualenvwrapper.sh
 
-# refactored this elsewhere
-# create a virtual environment (if needed)
-#[[ ! -d venv.vagrant/ ]] && virtualenv venv.vagrant
+mkvirtualenv -a $MDID_DIR -r $MDID_DIR/requirements.txt mdid 1>> bootstrap_logs/venv_install.txt 2>> bootstrap_logs/venv_install_errors.txt
+workon mdid
 
-# enter our virtual environment
-#source venv.vagrant/bin/activate
+echo `which python`
+# install our requirements in case the above one fails... ?
+# deactivate; workon mdid; pip install --upgrade -r requirements.txt 1>> bootstrap_logs/install.txt 2>> bootstrap_logs/install_errors.txt
 
-
-# move into our project dir
-cd $MDID_DIR
-
-# install our requirements
-pip install --upgrade -r requirements.txt
+sudo chown -R vagrant:vagrant /home/vagrant/
 
 ##############################################################################
 # Configure MDID
 ##############################################################################
-# create the MDID database -
-mysql -u root -p$SQL_PASSWORD < /vagrant/.vagrant_provision/create_database.sql
+### alternative - run not in a script
+#$MY_SQLS -e  "DROP DATABASE IF EXISTS rooibos;"
+#$MY_SQLS -e  "CREATE DATABASE IF NOT EXISTS rooibos CHARACTER SET utf8;"
+#$MY_SQLS -e  "GRANT ALL PRIVILEGES ON rooibos.* TO rooibos@localhost IDENTIFIED BY 'rooibos';"
+#$MY_SQLS -e  "UPDATE mysql.user SET  Select_priv='Y', Insert_priv='Y',
+#              Update_priv='Y', Delete_priv='Y', Create_priv='Y', Drop_priv='Y',
+#              Index_priv='Y', Alter_priv='y'
+#              WHERE Host='localhost' AND User='rooibos';"
+#$MY_SQLS -e  "FLUSH PRIVILEGES;"
 
 # you can load the schema to work around current weird migrations problems
 # mysql -u root -p$SQL_PASSWORD < /vagrant/.vagrant_provision/mdid_schema.sql
 
-# not sure why, but this was deleting my settings_local file and then failing
-# - this might be a better thing with environment variables
-# todo: look
 
 ## save settings_local.py from template if there is none
 if [ ! -f $CONFIG_DIR/settings_local.py ]; then
@@ -190,36 +208,40 @@ else
   echo "~~~ settings_local.py exists so using it, \e[91m make sure your settings are correct \e[21m ~~~"
 fi
 
-# this is clever, but I broke it - see VAGRANT_GATEWAY in settings_local.py to see if I was able to re-un-break it
+### replaced this in settings_local_template.py so that we don't accidently mess up someone's settings_local.py
 ## Get the default gateway IP address so we can add it to INTERNAL_IPS
-GATEWAY_IP=`route -n | grep 'UG' | awk '{print $2}'`
-## filter our local settings template, replacing necessary values
-cat $CONFIG_DIR/settings_local.py | sed -e "s/<<GATEWAY_IP>>/$GATEWAY_IP/" > $CONFIG_DIR/settings_local.py
+#GATEWAY_IP=`route -n | grep 'UG' | awk '{print $2}'`
+### backup settings_local.py
+#cp -n $CONFIG_DIR/settings_local.py $CONFIG_DIR/settings_loc.backup.py
+### filter our local settings template, replacing necessary values
+#cat $CONFIG_DIR/settings_local.py | sed -e "s/<<GATEWAY_IP>>/$GATEWAY_IP/" > $CONFIG_DIR/settings_local.py
 
-# move into the $MDID directory (django 1.6 now has manage.py in the top dir)
-cd $MDID_DIR
+mysql --user=root --password=$SQL_PASSWORD -v < /vagrant/.vagrant_provision/create_database.sql 1>> bootstrap_logs/mdid_install.txt 2>> bootstrap_logs/mdid_install_errors.txt
+mysql --user=rooibos --password=$SQL_PASSWORD -v -e "show grants; show tables rooibos;" 1>> bootstrap_logs/mdid_install.txt 2>> bootstrap_logs/mdid_install_errors.txt
+
 # make manage.py executable like "./mangage.py syncdb"
-chmod +x manage.py
+chmod +x $MDID_DIR/manage.py
 
-# setup the database
+# setup the app using full paths to the correct python and correct manage.py
+MDID_PY=$WORKON_HOME/mdid/bin/python
 
-# ./manage.py migrate --fake-initial
-./manage.py migrate
-./manage.py createcachetable cache
-
+# create rooibos specific tables
+$MDID_PY $MDID_DIR/manage.py migrate rooibos --noinput  1>> bootstrap_logs/mdid_install.txt 2>> bootstrap_logs/mdid_install_errors.txt
+# make sure all apps (including unmigrated apps) are processed
+$MDID_PY $MDID_DIR/manage.py migrate --noinput 1>> bootstrap_logs/mdid_install.txt 2>> bootstrap_logs/mdid_install_errors.txt
+# copy static files from all the apps into settings.STATIC_DIR
+$MDID_PY $MDID_DIR/manage.py collectstatic --noinput 1>> bootstrap_logs/mdid_install.txt 2>> bootstrap_logs/mdid_install_errors.txt
 
 echo -e "################# Adding Upstart scripts for Solr and the Workers #################"
 cp $PROVISION_DIR/mdid3-*.conf /etc/init
 
+sudo chown -R vagrant:vagrant /home/vagrant/
 
 echo -e "################# Starting Up Services #################"
 # start up the services
 service mdid3-solr start
 service mdid3-workers start
 
-
-
 echo -e "### COMPLETED ###"
 echo -e "You can start the mdid server from your desktop via"
-echo -e "vagrant ssh -c '/vagrant/manage.py runserver'"
-
+echo -e "   vagrant ssh -c \"~/runserver\""
